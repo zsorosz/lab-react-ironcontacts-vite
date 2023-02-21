8b71c848a19e24d12ff92799144f9e745bb3d7f2
zsorosz <oroszi.zsofi@gmail.com>
2023-02-21 16:14:01 +0100

    Iteration 4-5 done

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,82 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import contacts from "./contacts.json";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [contactList, setContactList] = useState(contacts.slice(0, 5));
+  const [remainingContacts, setRemainingContacts] = useState(contacts.slice(6));
+
+  const addRandom = () => {
+    let randomIndex = Math.floor(Math.random() * remainingContacts.length);
+    let randomContact = remainingContacts[randomIndex];
+    setContactList([...contactList, randomContact]);
+    let filteredArr = remainingContacts.filter((el, index) => {
+      return el.name !== randomContact.name;
+    });
+    setRemainingContacts(filteredArr);
+    console.log(randomIndex, randomContact);
+  };
+  const sortByName = () => {
+    const sortedList = contactList.sort((a, b) => {
+      if (a.name < b.name) {
+        return -1;
+      }
+      if (a.name > b.name) {
+        return 1;
+      }
+    });
+    setContactList([...sortedList]);
+  };
+  const sortByPopularity = () => {
+    const sortedList = contactList.sort((a, b) => b.popularity - a.popularity);
+    setContactList([...sortedList]);
+  };
+  const deleteContact = (event) => {
+    const id = event.target.id;
+    const filteredArr = contactList.filter((contact) => {
+      return contact.id !== id;
+    });
+    setContactList([...filteredArr]);
+  };
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Ironcontacts</h1>
+      <button onClick={addRandom}>Add Random Contact</button>
+      <button onClick={sortByPopularity}>Sort by Popularity</button>
+      <button onClick={sortByName}>Sort by Name</button>
+      <table>
+        <thead>
+          <tr>
+            <th>Picture</th>
+            <th>Name</th>
+            <th>Popularity</th>
+            <th>Won Oscar</th>
+            <th>Won Emmy</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {contactList.map((el) => (
+            <tr key={el.id}>
+              <td>
+                <img width="50" src={el.pictureUrl} alt="celeb" />
+              </td>
+              <td>{el.name}</td>
+              <td>{el.popularity.toFixed(2)}</td>
+              <td>{el.wonOscar ? "🏆" : ""}</td>
+              <td>{el.wonEmmy ? "🏆" : ""}</td>
+              <td>
+                <button id={el.id} onClick={deleteContact}>
+                  Delete
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
